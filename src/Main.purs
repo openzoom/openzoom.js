b6988c38c49e101cd14083773d5dd25bd81b6f93
Daniel Gasienica <daniel@gasienica.ch>
2017-09-06 00:14:53 -0400

    Animate fade-in of pyramid levels

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,19 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
+import Color (Color, rgb, toHexString)
+import Control.Monad.Eff (Eff, foreachE)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Timer (TIMER)
+import Data.Array ((!!), findIndex, mapWithIndex)
+import Data.Int (decimal, toStringAs, toNumber)
 import Data.Maybe (Maybe(Just, Nothing))
+import Debug.Trace (traceShow)
 import DOM (DOM)
-import Graphics.Canvas as C
 import Graphics.Canvas (CANVAS)
+import Graphics.Canvas as C
 import Signal (foldp, runSignal)
 import Signal.DOM (animationFrame)
-import Data.Int (decimal, toStringAs)
-import Color (Color, rgb, toHexString)
-import Data.Array ((!!))
 
 -- ADTs
 data ImagePyramid = ImagePyramid
@@ -54,7 +55,7 @@ main = do
     Just canvas -> do
       context <- C.getContext2D canvas
       frames <- animationFrame
-      let app = foldp (const update) initialState frames
+      let app = foldp (\ts state -> update state Render) initialState frames
       runSignal $ render context <$> app
     Nothing -> pure unit
 
@@ -70,30 +71,46 @@ loadImage (ImagePyramidLevel level) = C.tryLoadImage src callback
         Nothing -> pure unit
 
 type State =
-  { image :: ImagePyramid
-  , alpha :: Number
+  { image      :: ImagePyramid
+  , levelAlpha :: Array Number
   }
 
 initialState :: State
 initialState =
   { image : ImagePyramid
-    { width: 400
-    , height: 400
+    { width: 600
+    , height: 600
     , tileWidth: 200
     , tileHeight: 200
     , tileOverlap: 0
-    , levels: [
-        ImagePyramidLevel
-          { index: 0
-          , width: 400
-          , height: 400
-          , numColumns: 1
-          , numRows: 1
-          , color: rgb 255 128 0
-          }
-      ]
+    , levels:
+        [ ImagePyramidLevel
+            { index: 0
+            , width: 600
+            , height: 600
+            , numColumns: 3
+            , numRows: 3
+            , color: rgb 255 128 0
+            }
+        , ImagePyramidLevel
+            { index: 1
+            , width: 400
+            , height: 400
+            , numColumns: 2
+            , numRows: 2
+            , color: rgb 0 128 255
+            }
+        , ImagePyramidLevel
+            { index: 2
+            , width: 200
+            , height: 200
+            , numColumns: 1
+            , numRows: 1
+            , color: rgb 0 255 0
+            }
+        ]
     }
-  , alpha : 0.0
+  , levelAlpha: [0.0, 0.0, 0.0]
   }
 
 scene ::
@@ -111,8 +128,26 @@ scene =
   , color: rgb 0 0 0
   }
 
-update :: State -> State
-update state = state { alpha = clamp 0.0 1.0 (state.alpha + 0.05) }
+data Action =
+    Render
+  | LoadImage
+
+update :: State -> Action -> State
+update state action = traceShow state.levelAlpha $ \_ -> case action of
+  Render ->
+    -- Find lowest level with alpha < 1.0
+    let (ImagePyramid image) = state.image
+        mLowestTransparentLevelIndex = findIndex (_ < 1.0) state.levelAlpha in
+    case mLowestTransparentLevelIndex of
+      Just lowestIndex ->
+        state { levelAlpha = mapWithIndex (updateLevelAlpha lowestIndex) state.levelAlpha }
+      Nothing ->
+        state
+  _ -> state
+  where
+    updateLevelAlpha targetIndex index value
+      | index == targetIndex = clamp 0.0 1.0 (value + 0.025)
+      | otherwise = value
 
 render :: forall eff. C.Context2D -> State -> Eff (canvas :: CANVAS | eff) Unit
 render context state = do
@@ -129,15 +164,19 @@ clearCanvas ctx = do
 drawImage :: forall eff. C.Context2D -> State -> Eff (canvas :: CANVAS | eff) Unit
 drawImage ctx state = do
     let (ImagePyramid image) = state.image
-    case image.levels !! 0 of
-      (Just (ImagePyramidLevel level)) -> do
-        _ <- C.setFillStyle (toHexString level.color) ctx
-        _ <- C.setGlobalAlpha ctx state.alpha
-        _ <- C.fillRect ctx
-              { x: 0.0
-              , y: 0.0
-              , w: scene.width / 2.0
-              , h: scene.height / 2.0
-              }
-        pure unit
-      _ -> pure unit
+    -- Draw levels from lowest to highest
+    foreachE image.levels \(ImagePyramidLevel level) -> do
+      case state.levelAlpha !! level.index of
+        Just alpha -> do
+          _ <- C.setFillStyle (toHexString level.color) ctx
+          _ <- C.setGlobalAlpha ctx alpha
+          _ <- C.fillRect ctx
+                { x: 0.0
+                , y: 0.0
+                , w: toNumber level.width
+                , h: toNumber level.height
+                }
+          pure unit
+        Nothing ->
+          pure unit
+      pure unit
