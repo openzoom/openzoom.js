b64a895d8ff9ced738d26036eeb6815219fc8f86
Daniel Gasienica <daniel@gasienica.ch>
2017-09-05 20:34:52 -0400

    Render image pyramid based on state

--- src/Main.purs
+++ src/Main.purs
@@ -12,13 +12,10 @@ import Graphics.Canvas (CANVAS)
 import Signal (foldp, runSignal)
 import Signal.DOM (animationFrame)
 import Data.Int (decimal, toStringAs)
+import Color (Color, rgb, toHexString)
+import Data.Array ((!!))
 
-data ImagePyramidLevel = ImagePyramidLevel
-  { index  :: Int
-  , width  :: Int
-  , height :: Int
-  }
-
+-- ADTs
 data ImagePyramid = ImagePyramid
   { width  :: Int
   , height :: Int
@@ -28,6 +25,28 @@ data ImagePyramid = ImagePyramid
   , levels :: Array ImagePyramidLevel
   }
 
+data ImagePyramidLevel = ImagePyramidLevel
+  { index      :: Int
+  , width      :: Int
+  , height     :: Int
+  , numRows    :: Int
+  , numColumns :: Int
+  , color      :: Color
+  }
+
+data ImagePyramidTile = ImagePyramidTile
+  { width  :: Int
+  , bounds ::
+    { x :: Number
+    , y :: Number
+    , width :: Number
+    , height :: Number
+    }
+  , row    :: Int
+  , column :: Int
+  }
+
+-- Main
 main :: forall eff. Eff (canvas :: CANVAS, console :: CONSOLE, dom :: DOM, timer :: TIMER | eff) Unit
 main = do
   mcanvas <- C.getCanvasElementById "scene"
@@ -39,6 +58,7 @@ main = do
       runSignal $ render context <$> app
     Nothing -> pure unit
 
+-- Helper
 loadImage :: forall eff. ImagePyramidLevel -> Eff (canvas :: CANVAS, console :: CONSOLE | eff) Unit
 loadImage (ImagePyramidLevel level) = C.tryLoadImage src callback
   where
@@ -57,12 +77,21 @@ type State =
 initialState :: State
 initialState =
   { image : ImagePyramid
-    { width: 800
-    , height: 800
-    , tileWidth: 254
-    , tileHeight: 254
-    , tileOverlap: 1
-    , levels: [ImagePyramidLevel {index: 0, width: 800, height: 800}]
+    { width: 400
+    , height: 400
+    , tileWidth: 200
+    , tileHeight: 200
+    , tileOverlap: 0
+    , levels: [
+        ImagePyramidLevel
+          { index: 0
+          , width: 400
+          , height: 400
+          , numColumns: 1
+          , numRows: 1
+          , color: rgb 255 128 0
+          }
+      ]
     }
   , alpha : 0.0
   }
@@ -72,17 +101,15 @@ scene ::
   , y :: Number
   , width :: Number
   , height :: Number
+  , color :: Color
   }
 scene =
   { x: 0.0
   , y: 0.0
   , width: 800.0
   , height: 800.0
+  , color: rgb 0 0 0
   }
-
-data Action =
-    Render
-  | ImageLoaded
 
 update :: State -> State
 update state = state { alpha = clamp 0.0 1.0 (state.alpha + 0.05) }
@@ -95,18 +122,22 @@ render context state = do
 
 clearCanvas :: forall eff. C.Context2D -> Eff (canvas :: CANVAS | eff) Unit
 clearCanvas ctx = do
-  _ <- C.setFillStyle "#000000" ctx
+  _ <- C.setFillStyle (toHexString scene.color) ctx
   _ <- C.fillRect ctx { x: 0.0, y: 0.0, w: scene.width, h: scene.height }
   pure unit
 
 drawImage :: forall eff. C.Context2D -> State -> Eff (canvas :: CANVAS | eff) Unit
 drawImage ctx state = do
-  _ <- C.setFillStyle "#0088DD" ctx
-  _ <- C.setGlobalAlpha ctx state.alpha
-  _ <- C.fillRect ctx
-        { x: 0.0
-        , y: 0.0
-        , w: scene.width / 2.0
-        , h: scene.height / 2.0
-        }
-  pure unit
+    let (ImagePyramid image) = state.image
+    case image.levels !! 0 of
+      (Just (ImagePyramidLevel level)) -> do
+        _ <- C.setFillStyle (toHexString level.color) ctx
+        _ <- C.setGlobalAlpha ctx state.alpha
+        _ <- C.fillRect ctx
+              { x: 0.0
+              , y: 0.0
+              , w: scene.width / 2.0
+              , h: scene.height / 2.0
+              }
+        pure unit
+      _ -> pure unit
