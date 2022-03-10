a696149e810de237e3f1a7d6d59681929f6bd184
Daniel Gasienica <daniel@gasienica.ch>
2022-03-10 23:45:52 +0100

    Initial commit: TypeScript

---
+++ typescript/src/descriptors/DeepZoomImageDescriptor.ts
@@ -0,0 +1,122 @@
+export class DeepZoomImageDescriptor {
+  private url: string
+  private width: number
+  private height: number
+  private tileSize: number
+  private tileOverlap: number
+  private tileFormat: TileFormat
+
+  levels: Array<ImagePyramidLevel>
+  numLevels: number
+
+  constructor({
+    url,
+    width,
+    height,
+    tileSize,
+    tileOverlap,
+    tileFormat
+  }: {
+    url: string
+    width: number
+    height: number
+    tileSize: number
+    tileOverlap: number
+    tileFormat: TileFormat
+  }) {
+    this.url = url
+    this.width = width
+    this.height = height
+    this.tileSize = tileSize
+    this.tileOverlap = tileOverlap
+    this.tileFormat = tileFormat
+
+    this.numLevels = this.getNumLevels(width, height)
+  }
+
+  private getNumLevels(width: number, height: number): number {
+    return Math.ceil(Math.log(Math.max(width, height)) / Math.LN2) + 1
+  }
+  private createLevels(
+    originalWidth: number,
+    originalHeight: number,
+    tileWidth: number,
+    tileHeight: number,
+    numLevels: number
+  ): void {
+    var maxLevel: number = numLevels - 1
+
+    for (var index: number = 0; index <= maxLevel; index++) {
+      var size: Point = this.getSize(index)
+      var width: number = size.x
+      var height: number = size.y
+      var numColumns: number = Math.ceil(width / tileWidth)
+      var numRows: number = Math.ceil(height / tileHeight)
+      var level: ImagePyramidLevel = new ImagePyramidLevel(
+        this,
+        index,
+        width,
+        height,
+        numColumns,
+        numRows
+      )
+
+      // addLevel(level)
+      this.levels.push(level)
+    }
+  }
+
+  private getScale(level: number): number {
+    var maxLevel: number = this.numLevels - 1
+    // 1 / (1 << maxLevel - level)
+    return Math.pow(0.5, maxLevel - level)
+  }
+
+  private getSize(level: number): Point {
+    var size: Point = new Point()
+    var scale: number = this.getScale(level)
+    size.x = Math.ceil(width * scale)
+    size.y = Math.ceil(height * scale)
+
+    return size
+  }
+}
+
+type TileFormat = "jpg" | "jpeg" | "png"
+
+class ImagePyramidLevel {
+  descriptor: DeepZoomImageDescriptor
+  index: number
+  width: number
+  height: number
+  numColumns: number
+  numRows: number
+
+  constructor(
+    descriptor: DeepZoomImageDescriptor,
+    index: number,
+    width: number,
+    height: number,
+    numColumns: number,
+    numRows: number
+  ) {
+    this.descriptor = descriptor
+
+    this.index = index
+    this.width = width
+    this.height = height
+    this.numColumns = numColumns
+    this.numRows = numRows
+  }
+}
+
+class Point {
+  x: number
+  y: number
+
+  // FIXME: Avoid defaults
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x
+    this.y = y
+  }
+}
---
+++ typescript/src/index.ts
@@ -0,0 +1,122 @@
+// --- Helpers -----------------------------------------------------------------
+
+const clamp = (value, min, max) => {
+  if (value < min) {
+    return min
+  }
+
+  if (value > max) {
+    return max
+  }
+
+  return value
+}
+
+// --- Setup -------------------------------------------------------------------
+
+const scene = document.getElementById("image")
+
+if (!(scene instanceof HTMLCanvasElement)) {
+  throw new Error("`scene` must be a <canvas>")
+}
+
+const context = scene.getContext("2d")
+
+if (!context) {
+  throw new Error("Couldn’t instantiate canvas context.")
+}
+
+context.fillStyle = "black"
+context.fillRect(0, 0, scene.width, scene.height)
+
+// --- State -------------------------------------------------------------------
+
+const statesHistory = []
+let state = {
+  levels: []
+}
+
+const getState = () => state
+const setState = (newState) => {
+  statesHistory.push(state)
+  state = newState
+}
+
+const update = (currentState, action) => {
+  switch (action.type) {
+    case "imageLoaded": {
+      const { image, level: levelIndex } = action.payload
+      const newLevels = [...currentState.levels]
+      const level = currentState.levels[levelIndex] || {}
+      newLevels[levelIndex] = { ...level, image, loaded: true }
+
+      return { ...currentState, levels: newLevels }
+    }
+
+    default:
+      throw new Error(`Unknown action type: ${action.type}`)
+  }
+}
+
+// --- Preloading --------------------------------------------------------------
+
+/*eslint-disable no-magic-numbers */
+const LEVELS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+/*eslint-enable no-magic-numbers */
+
+LEVELS.forEach((level) => {
+  const image = new Image()
+  image.src = `http://cache.zoomhub.net/content/0w5YD_files/${level}/0_0.jpg`
+  image.onload = () => {
+    const newState = update(getState(), {
+      type: "imageLoaded",
+      payload: { image, level }
+    })
+    setState(newState)
+  }
+})
+
+// --- Rendering ---------------------------------------------------------------
+
+const LEVEL_BLEND_DURATION = 100
+let lastTimestamp = null
+let levelIndex = 0
+
+const step = (timestamp) => {
+  if (lastTimestamp === null) {
+    lastTimestamp = timestamp
+  }
+
+  const elapsedTime = timestamp - lastTimestamp
+  const isLevelLoaded =
+    state.levels[levelIndex] && state.levels[levelIndex].loaded
+
+  if (isLevelLoaded) {
+    const levelImage = state.levels[levelIndex].image
+    const alpha = clamp(elapsedTime / LEVEL_BLEND_DURATION, 0, 1)
+    context.globalAlpha = alpha
+
+    /*eslint-disable id-length*/
+    const sx = 0
+    const sy = 0
+    const sw = levelImage.width
+    const sh = levelImage.height
+
+    const dx = 0
+    const dy = 0
+    const dw = scene.width
+    const dh = scene.height
+    /*eslint-enable id-length*/
+
+    context.drawImage(levelImage, sx, sy, sw, sh, dx, dy, dw, dh)
+
+    if (alpha === 1 && levelIndex < LEVELS.length - 1) {
+      lastTimestamp = timestamp
+      levelIndex += 1
+    }
+  }
+
+  window.requestAnimationFrame(step)
+}
+
+window.requestAnimationFrame(step)
